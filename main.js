96e4aa089ee6a57dc0f41c4b491a95788460f4d1
Jamie Tater <jtater1@gmail.com>
2020-02-11 18:32:29 -0500

    changed logic

--- main.js
+++ main.js
@@ -1,84 +1,57 @@
 const STORE = QUESTIONS;
 let ASKED = [];
 let ansSet = [];
+let rightWrong = [0,0];
 // console.log(STORE[9].Answers[2].Feedback);
     
 // render the start page
-$(
 // Button should trigger quiz questions to render
     function handleStartButton(){
-        $('button').on('click', function(){
         console.log('handleStartButton ran');
-        if(ASKED.length === STORE.length){
-            renderResult();
-        }
-        if($(this).hasClass('in-progress')){
-            renderQuiz();
-        }
-        else {$('h1').empty();
-            $('p').remove();
-            $(this).addClass('in-progress');
-            $('js-start').text('Submit');
-            $('h1').text(`Question ${ASKED.length} out of ${STORE.length}`);
-            $('h1').after(`<ul class="progress-key">
-                <li class="progress-item progress-number"># right</li>
-                <li class="progress-item">
-                    <figure>
-                    <img src="./images/check-mark.png" alt="check mark right" height="30" width="30">
-                    </figure>
-                </li>
-                <li class="progress-item progress-number"># wrong</li>
-                <li class="progress-item">
-                    <figure>
-                    <img src="./images/x.png" alt="x mark wrong" height="30" width="30">
-                    </figure>
-                </li>
-            </ul>`);
-            renderQuiz();
-        }
+        $('.js-start').click(function(){
+            $('.start-page').addClass('hidden');
+            renderQuiz(getQuizQuestion, randomAnsSet);
+            $('.question-page').removeClass('hidden');
+            handleSubmitButton();
         })
-    });
+    }
+    // handle Submit behavior
+    function handleSubmitButton(){
+        $('.js-submit').click(function(event){
+           console.log('handleSubmitButton ran');
+           let selectedAns = $('input[name=answers]:checked').val();
+           isAnsCorrect(ansSet[selectedAns]);
+           $('.question-page').addClass('hidden');
+           $('.feedback-page').removeClass('hidden');
+           $('h1').text(`Question ${ASKED.length} out of ${STORE.length}`);
+        })
+    }
+    function handleTryAgain (){
+        console.log('handleTryAgain ran');
+    }
 // render the quiz 
-function renderQuiz(){
+function renderQuiz(question, answers){
     console.log('renderQuiz ran');
-    let pickedQ = getQuizQuestion();
+    let pickedQ = question(STORE, ASKED);
     ASKED.push(pickedQ);
     $('h1').text(`Question ${ASKED.length} out of ${STORE.length}`);
-    randomAnsSet(pickedQ);
-    randomAnsSet(pickedQ);
-    randomAnsSet(pickedQ);
-    randomAnsSet(pickedQ);
-    $('main').html(`<form>
-    <legend class="question">${pickedQ.Question}</legend>
-    <ul class="container">
-        <li class="box">
-            <input type="radio" name="answers" id="answer-A" value="0">
-            <label for="answers">${ansSet[0].Answer}</label>
-        </li>
-        <li class="box">
-            <input type="radio" name="answers" id="answer-B" value="1">
-            <label for="answers">${ansSet[1].Answer}</label>
-        </li>
-        <li class="box">
-            <input type="radio" name="answers" id="answer-C" value="2">
-            <label for="answers">${ansSet[2].Answer}</label>
-        </li>
-        <li class="box">
-            <input type="radio" name="answers" id="answer-D" value="3">
-            <label for="answers">${ansSet[3].Answer}</label>
-        </li>
-    </ul>
-</form>
-<section>
-            <button type="button" class="js-submit">Submit</button>
-        </section>`);
-    
+    $('.progress-right').text(rightWrong[0]);
+    $('.progress-wrong').text(rightWrong[1]);
+    answers(pickedQ);
+    answers(pickedQ);
+    answers(pickedQ);
+    answers(pickedQ);
+    $('.question').html(`${pickedQ.Question}`);
+    $('#A').html(`${ansSet[0].Answer}`);
+    $('#B').html(`${ansSet[1].Answer}`);
+    $('#C').html(`${ansSet[2].Answer}`);
+    $('#D').html(`${ansSet[3].Answer}`);
     }
     // get questions at random
-    function getQuizQuestion(){
+    function getQuizQuestion(hardCopy, askedQ){
     console.log('getQuizQuestion ran');
-    let newQ = STORE[Math.floor(Math.random()*STORE.length)];
-    if(ASKED.includes(newQ)){
+    let newQ = hardCopy[Math.floor(Math.random()*hardCopy.length)];
+    if(askedQ.includes(newQ)){
         getQuizQuestion();
     }
         else{ 
@@ -87,29 +60,39 @@ function renderQuiz(){
     }
     // get random answer set for question
     function randomAnsSet(questionInfo){
-        console.log('randomAnsSet ran')
-        let A = questionInfo.Answers[Math.floor(Math.random()*questionInfo.Answers.length)];
-        if(ansSet.includes(A)){
-            randomAnsSet();
-        }else{
-            ansSet.push(A);
-            return;
+        console.log('randomAnsSet ran');
+        let anAns = questionInfo.Answers[Math.floor(Math.random()*questionInfo.Answers.length)];
+        if(ansSet.includes(anAns)){
+            randomAnsSet(questionInfo);
         }
-    }
+            else if(ansSet.length !== questionInfo.Answers.length){ 
+                ansSet.push(anAns);
+            };
+        }
+
     // require a selected answer
     function checkIfAnsSelected(){
-    console.log('checkIfAnsSelected ran');
+        
+        if(radioValue){
+            console.log(radioValue);
+        }
     }
     function // check for correct answer
-    isAnsCorrect(){
+    isAnsCorrect(answer){
    console.log('isAnsCorrect ran');
+   if(answer.correct){
+
+   }
     }
     function // provide feedback based on answer
     giveFeedback(){
     console.log('giveFeedback ran');
     }
 
-// render the results
-function renderResult(){
-    console.log('renderResult ran');
-    }
+function handleQuiz(){
+    handleStartButton();
+    handleSubmitButton();
+    handleTryAgain();
+}
+
+$(handleQuiz);
